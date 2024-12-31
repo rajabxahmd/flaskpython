5beb6ce9e54b232c0b6eac6c56ec483c56a61073
Ahmed Rajab <rajabxamd.tva@gmail.com>
2024-12-31 10:59:43 +0530

    added all routes

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import math
 
 app = Flask(__name__)
 
@@ -9,5 +10,24 @@ def addition():
     result = int(num1) + int(num2)
     return jsonify({"result": result})
 
+@app.route("/factorial")
+def factorial():
+    num=request.args.get('num')
+    return jsonify({'result': math.factorial(num)})
+
+@app.route("/circlearea")
+def circlearea():
+    radius=request.args.get('radius')
+    return jsonify({'result': math.pi * float(radius) ** 2})
+
+@app.route("/palindrome")
+def palindrome():
+    text=request.args.get('text')
+    result = text == text[::-1]
+    if result:
+        return jsonify({'the text is a palindrome'})
+    else:
+        return jsonify({'the text is not a palindrome'})
+    
 if __name__ == "__main__":
     app.run(debug=True)
